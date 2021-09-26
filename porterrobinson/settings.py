1b12670b965b73f30ed45c036c42157e3030f93c
utkrishtkumar <48125146+utkrishtkumar@users.noreply.github.com>
2021-09-27 04:34:29 +0530

    d

--- porterrobinson/settings.py
+++ porterrobinson/settings.py
@@ -7,7 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.2/topics/settings/
 
 For the full list of settings and their values, see
-https://docs.djangoproject.com/en/3.2/ref/settings/
+https://docs.djangoproject.com/en/3.2/re
 """
 import os
 import sys
